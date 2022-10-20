d629811c17c50899512497c9e20c27fbc1cb3d0f
gglsilva <gabrielchamex@gmail.com>
2022-10-20 16:53:08 -0300

    feat(views): add cart_remove, cart_detail and urls config

--- apps/cart/pages/views.py
+++ apps/cart/pages/views.py
@@ -2,8 +2,8 @@ from itertools import product
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from shop.models import Product
-from .cart import Cart
-from .forms import CartAddProductForm
+from ..cart import Cart
+from ..forms import CartAddProductForm
 
 
 @require_POST
@@ -17,3 +17,16 @@ def cart_add(request, product_id):
                 quantity=cd['quantity'],
                 override_quantity=cd['override'])
     return redirect('cart:cart_detail')
+
+
+@require_POST
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart:cart_detail')
+
+
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, 'cart/detail.html', {'cart': cart})

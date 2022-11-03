2b17f6672dee651b500b0ad6ffdd50a67de80a8a
gglsilva <gabrielchamex@gmail.com>
2022-11-02 22:05:29 -0300

    bug(cart):remove cart bug

--- apps/cart/cart.py
+++ apps/cart/cart.py
@@ -63,7 +63,9 @@ class Cart(object):
         """
         Remove a product from the cart.
         """
-        product_id = str(product_id)
+
+        product_id = str(product.id)
+        print(product_id)
         if product_id in self.cart:
             del self.cart[product_id]
             self.save()
--- apps/cart/pages/views.py
+++ apps/cart/pages/views.py
@@ -33,5 +33,5 @@ def cart_detail(request):
         item['update_quantity_form'] = CartAddProductForm(initial={
                                         'quantity': item['quantity'],
                                         'override': True})
-                                            
+
     return render(request, 'cart/detail.html', {'cart': cart})
